a62e75ea52c3144aeae11c70c19b9b94a9df853e
Hemanth <hem@pop-os.localdomain>
2026-03-08 21:31:14 -0600

    linked list solutions

--- 3.linked_list/1.reverse_linked_list.py
+++ 3.linked_list/1.reverse_linked_list.py
@@ -1,9 +1,9 @@
 class ReverseLinkedList:
     def reverse(self, head):
-        # reverse direction of arrow pointing in the linked list
+        #reverse direction of arrow pointing in the linked list
         curr, prev = head, None
         while curr:
-            # keep track of next node not to break link with next element
+            #keep track of next node; not to break link with next element
             next_node = curr.next
             curr.next = prev
             prev = curr
