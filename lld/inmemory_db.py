511f3f47a3a54cfc4ba1085a04dc870857cf02a6
hkr691 <hkr691@gmail.com>
2026-03-23 12:39:12 -0600

    level 3 of inmemory db

--- lld/inmemory_db.py
+++ lld/inmemory_db.py
@@ -4,7 +4,7 @@ class InMemoryDB:
   def __init__(self):
     self.db = {}
   
-  def _expire_if_needed(self, ts: int, key: str, field: str):'
+  def _expire_if_needed(self, ts: int, key: str, field: str):
     if key in self.db and field in self.db[key]:
       val, expire_at = self.db[key][field]
       if ts >= expire_at:
@@ -20,8 +20,8 @@ class InMemoryDB:
     
     self.db[key][field] = (value, float('inf'))
   
-  def set_with_ttl(self, key: str, field: str, ttl: int):
-    if key not in self/db:
+  def set_with_ttl(self, ts: int, key: str, field: str, value: int, ttl: int):
+    if key not in self.db:
       self.db[key] = {}
     
     self.db[key][field] = (value, ts + ttl)
@@ -38,7 +38,7 @@ class InMemoryDB:
     return False
   
   def compare_and_delete(self, ts: int, key: str, field: str, expected_value: int):
-    self._exipre_if_needed(ts, key, field)
+    self._expire_if_needed(ts, key, field)
     if key not in self.db or field not in self.db[key]:
       return False
     current_value = self.db[key][field][0]
@@ -59,12 +59,22 @@ class InMemoryDB:
   def scan(self, ts: int, key: str):
     if key not in self.db:
       return []
+    fields = list(self.db[key].keys())
+    for field in fields:
+      self._expire_if_needed(ts, key, field)
+    if key not in self.db:
+      return []
     sorted_fields = sorted(self.db[key].keys())
-    return [f"{field}({self.db[key][field]})" for field in sorted_fields]
+    return [f"{field}({self.db[key][field][0]})" for field in sorted_fields]
   
   def scan_by_prefix(self, ts: int, key: str, prefix: str):
     if key not in self.db:
       return []
+    fields = list(self.db[key].keys())
+    for field in fields:
+      self._expire_if_needed(ts, key, field)
+    if key not in self.db:
+      return []
     valid_fields = [field for field in self.db[key].keys() if field.startswith(prefix)]
     sorted_fields = sorted(valid_fields)
-    return [f"{field}({self.db[key][field]})" for field in sorted_fields]
+    return [f"{field}({self.db[key][field][0]})" for field in sorted_fields]
