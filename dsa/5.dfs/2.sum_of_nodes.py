e1795f52aa5bdbc10e9089e8778f5a9b42587f50
hkr691 <hkr691@gmail.com>
2026-04-07 16:05:29 -0600

    nodes sum dfs, bfs, iter

--- dsa/5.dfs/2.sum_of_nodes.py
+++ dsa/5.dfs/2.sum_of_nodes.py
@@ -1,5 +1,36 @@
+from collections import deque
 class SumOfNodes:
     def sum_of_tree_nodes(self, node):
         if not node:
             return 0
         return node.val + self.sum_of_tree_nodes(node.left) + self.sum_of_tree_nodes(node.right)
+        
+    def sum_of_nodes_iterative(self, root):
+        if not root:
+            return 0
+        stack = [root]
+        total_sum = 0
+        
+        while stack:
+            node = stack.pop()
+            total_sum += node.val
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return total_sum
+    
+    def sum_of_nodes_bfs(self, root):
+        if not root:
+            return 0
+        queue = deque([root])
+        total_sum = 0
+        
+        while queue:
+            node = queue.popleft()
+            total_sum += node.val
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return total_sum
