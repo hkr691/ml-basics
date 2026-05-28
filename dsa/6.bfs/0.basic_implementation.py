62dbb31ee59f7612540dcac803b8f9d0d572d445
hkr691 <hkr691@gmail.com>
2026-05-28 13:00:49 -0600

    bfs implementation level order traversal

--- dsa/6.bfs/0.basic_implementation.py
+++ dsa/6.bfs/0.basic_implementation.py
@@ -17,3 +17,23 @@ def bfs(root):
       queue.append(curr_node.right)
     
   return result
+
+def level_order(root):
+  if not root:
+    return []
+  
+  queue = deque([root])
+  res = []
+  
+  while queue:
+    curr_level = []
+    for _ in range(len(queue)):
+      curr_node = queue.popleft()
+      curr_level.append(curr_node.val)
+      
+      if curr_node.left:
+        queue.append(curr_node.left)
+      if curr_node.right:
+        queue.append(curr_node.right)
+    res.append(curr_level)
+  return res
